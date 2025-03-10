e5744c69b37201f4d6aea7dbc413f6e96d52c07e
Mateus Rocha <mateus.rocha1rj@outlook.com>
2025-03-10 11:44:03 -0400

    wait, what?

--- app/film/layout.js
+++ app/film/layout.js
@@ -1,29 +1,38 @@
-"use client"; // Permite manipulação de estado no Next.js
-
-import { useState } from "react";
-import "./categoria.css";
 import Link from "next/link";
+import "./categoria.css"; // Importando o CSS corretamente
 
 export default function CategoriaLayout({ children }) {
   return (
-    <div>
-      <header id="header">
-        <nav id="nav">
-          <button id="menu-toggle">☰</button>
-          <div id="nav-links">
-            <Link href="/">Home</Link>
-            <Link className="selected" href="#">
-              Film
-            </Link>
-            <Link href="/design">Design</Link>
-            <Link href="/photo">Photography</Link>
-            <Link href="/brands">Brands</Link>
-            <Link href="/myself">Myself</Link>
-          </div>
-        </nav>
-        <h1>Film</h1>
+    <div className="categoria-container">
+      <header>
+        <h1>FILM</h1>
         <p>Short-films, VideoClips, Documentaries, Ads, and more!</p>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/design" className="nav-link">
+                Design
+              </Link>
+            </li>
+            <li>
+              <Link href="/brands" className="nav-link">
+                Brands
+              </Link>
+            </li>
+            <li>
+              <Link href="/photo" className="nav-link">
+                Photography
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <hr />
       <main>{children}</main>
     </div>
   );
