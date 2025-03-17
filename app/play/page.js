d1b7ea428373d4c947f7b52700c27f434cf31691
Mateus Rocha <mateus.rocha1rj@outlook.com>
2025-03-17 14:57:26 -0400

    fixing environment problems

--- app/play/page.js
+++ app/play/page.js
@@ -1,15 +1,17 @@
 import Link from "next/link";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+
 export default async function UsersPage() {
-  const res = await fetch("/api/play");
+  const res = await fetch(`${API_URL}/api/play`);
   const users = await res.json();
+
   return (
     <div>
       <h1>Últimos usuários</h1>
       {users.map((links) => (
         <div key={links.id}>
           <Link
-            key={links.id}
             href={`https://meublog.com/posts/${links.name}`}
             className="user-card"
           >
