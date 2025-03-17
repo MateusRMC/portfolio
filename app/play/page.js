4e0d8649472130871fa2e2db020c59f5d694076c
Mateus Rocha <mateus.rocha1rj@outlook.com>
2025-03-17 14:36:39 -0400

    playground environment

---
+++ app/play/page.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default async function UsersPage() {
+  const res = await fetch("http://localhost:3000/api/play");
+  const users = await res.json();
+  return (
+    <div>
+      <h1>Últimos usuários</h1>
+      {users.map((links) => (
+        <div key={links.id}>
+          <Link
+            key={links.id}
+            href={`https://meublog.com/posts/${links.name}`}
+            className="user-card"
+          >
+            {links.name}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
