ef39ee833f3adfdbe72c3837b5a21040641eda15
Mateus Rocha <mateus.rocha1rj@outlook.com>
2025-03-18 00:53:15 -0400

    styles and styles

--- app/page.js
+++ app/page.js
@@ -2,7 +2,29 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import "./styles/home.css";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="Links">
+      <h1>
+        <Link href="/">Home</Link>
+      </h1>
+      <h1>
+        <Link href="/film">Film</Link>
+      </h1>
+      <h1>
+        <Link href="/design">Design</Link>
+      </h1>
+      <h1>
+        <Link href="/photo">Photo</Link>
+      </h1>
+      <h1>
+        <Link href="/brands">Brands</Link>
+      </h1>
+      <h1>
+        <Link href="/about">About</Link>
+      </h1>
+    </div>
+  );
 }
