b3b8b50898b2018e85c2b052ef5ab4ae6b97f778
Mateus Rocha <mateus.rocha1rj@outlook.com>
2025-03-19 13:18:12 -0400

    resolvi usar notion pro repositório de case study de design e tal

---
+++ app/design/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Design() {
+  return (
+    <Link
+      href="https://mateusrmc.notion.site/Portfolio-Journal-1bbc21c96df5805fba05ca17072fc0ac?pvs=4"
+      target="_blank"
+    >
+      GO TO PROJECT
+    </Link>
+  );
+}
